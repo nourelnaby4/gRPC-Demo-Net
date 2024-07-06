c9bd9c739b6be2426aa8ecb796b94732b662c2a3
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-07-07 00:51:39 +0300

    feate: add Tracking Service

---
+++ GerpcServer/Services/TrackingService.cs
@@ -0,0 +1,27 @@
+﻿using GerpcServer.Protos;
+using Grpc.Core;
+using System.Text;
+
+namespace GerpcServer.Services;
+
+public class TelemetryTrackingService : TrackingService.TrackingServiceBase
+{
+    private readonly ILogger<TelemetryTrackingService> _logger;
+    public TelemetryTrackingService(ILogger<TelemetryTrackingService> logger)
+    {
+        _logger = logger;
+    }
+    public override Task<TrackingResponse> SendMessage(TrackingMessage request, ServerCallContext context)
+    {
+        var information = new StringBuilder();
+
+        information.AppendLine($"deviceId {request.DeviceId}: {request.DeviceId}");
+        information.AppendLine($"location lat{request.Location.Lat}, long:{request.Location.Long}");
+        information.AppendLine($"speed {request.Speed}");
+        information.AppendLine($"datetime {request.Stamp}");
+
+        _logger.LogInformation(information.ToString());
+
+        return Task.FromResult(new TrackingResponse { Success = true });
+    }
+}
