c6cf72c4935cb887a39fcfa1636204b8eb05f6de
AhmedNourElnabi <91994457+nourelnaby4@users.noreply.github.com>
2024-07-08 23:54:04 +0300

    chore: create channel between server and client

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -1,7 +1,15 @@
 using GrpcClient;
 
 var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+
+Console.WriteLine("Enter Device ID:");
+var deviceId = Int32.Parse(Console.ReadLine());
+
+builder.Services.AddHostedService<Worker>(provider =>
+{
+    var logger = provider.GetService<ILogger<Worker>>();
+    return  new Worker(logger, deviceId);
+});
 
 var host = builder.Build();
 host.Run();
--- GrpcClient/Worker.cs
+++ GrpcClient/Worker.cs
@@ -1,22 +1,52 @@
+using GerpcServer.Protos;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
+
 namespace GrpcClient;
 
 public class Worker : BackgroundService
 {
     private readonly ILogger<Worker> _logger;
+    private readonly int _deviceId;
+    private TrackingService.TrackingServiceClient _client;
 
-    public Worker(ILogger<Worker> logger)
+    private TrackingService.TrackingServiceClient Client
+    {
+        get
+        {
+            if (_client is null)
+            {
+                var channel = GrpcChannel.ForAddress("http://localhost:5240");
+                _client = new TrackingService.TrackingServiceClient(channel);
+            }
+            return _client;
+        }
+    }
+
+    public Worker(ILogger<Worker> logger, int deviceId)
     {
         _logger = logger;
+        _deviceId = deviceId;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        Random random = new Random();
         while (!stoppingToken.IsCancellationRequested)
         {
-            if (_logger.IsEnabled(LogLevel.Information))
+            var requset = new TrackingMessage
             {
-                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
+                DeviceId = _deviceId,
+                Speed = random.Next(0, 220),
+                Location = new Location { Lat = random.Next(0, 100), Long = random.Next(50, 200) },
+                Stamp = Timestamp.FromDateTime(DateTime.UtcNow),
+                Sensor = new Sensor { Key = "temp", Value = 30 }
+            };
+
+            var response = await Client.SendMessageAsync(requset);
+
+            _logger.LogInformation($"Response: {response.Success}");
+
             await Task.Delay(1000, stoppingToken);
         }
     }
